a61195a850eee8a37c1d3009f3512eb44aa2b9e2
MoisesSoares <98667100+MoisesSoares@users.noreply.github.com>
2025-09-17 16:33:54 -0300

    Portifolio update
    Portifolio update

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ document.getElementById("contactForm").addEventListener("submit", function(e) {
   alert("Obrigado pela mensagem! Em breve entrarei em contato.");
   this.reset();
 });
+document.querySelectorAll(".video-container").forEach(container => {
+  container.addEventListener("click", function () {
+    const videoId = this.getAttribute("data-id");
+    this.innerHTML = `
+      <iframe width="100%" height="100%" 
+        src="https://www.youtube.com/embed/${videoId}?autoplay=1&vq=hd1080" 
+        title="YouTube video player" 
+        frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+        allowfullscreen>
+      </iframe>
+    `;
+  });
+});
